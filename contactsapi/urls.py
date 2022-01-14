a00a44852f990fec6d00ebb0c715c500bf92fd41
unknown <eske17.ea@gmail.com>
2022-01-14 12:11:47 -0800

    remove redoc

--- contactsapi/urls.py
+++ contactsapi/urls.py
@@ -23,18 +23,18 @@ from drf_yasg import openapi
 
 ...
 
-schema_view = get_schema_view(
-   openapi.Info(
-      title="Contact list API",
-      default_version='v1',
-      description="An api for contacts",
-      terms_of_service="https://yourco/terms/",
-      contact=openapi.Contact(email="contact@contacts.remote"),
-      license=openapi.License(name="Test License"),
-   ),
-   public=True,
-   permission_classes=[permissions.AllowAny],
-)
+# schema_view = get_schema_view(
+#    openapi.Info(
+#       title="Contact list API",
+#       default_version='v1',
+#       description="An api for contacts",
+#       terms_of_service="https://yourco/terms/",
+#       contact=openapi.Contact(email="contact@contacts.remote"),
+#       license=openapi.License(name="Test License"),
+#    ),
+#    public=True,
+#    permission_classes=[permissions.AllowAny],
+# )
 
 #    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
 #    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
@@ -45,8 +45,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('authentication.urls')),
     path('api/contacts/', include('contacts.urls')),
-    path('', schema_view.with_ui('swagger',
-                                 cache_timeout=0), name='schema-swagger-ui'),
+    # path('', schema_view.with_ui('swagger',
+    #                              cache_timeout=0), name='schema-swagger-ui'),
     # path("redoc", schema_view.with_ui('redoc',
     #                                   cache_timeout=0), name='schema-redoc'),
 ]
